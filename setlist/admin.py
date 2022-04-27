b844563b46a3494c0f8903d9eae26aec26066a8a
mjjstockman <mjjstockman@gmail.com>
2022-04-27 14:08:43 +0000

    Add summernote

--- setlist/admin.py
+++ setlist/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Gig, Song
 
-# Register your models here.
+admin.site.register(Gig)
+admin.site.register(Song)
--- setlistsharer/urls.py
+++ setlistsharer/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # when want to add summernote to a field, watch 5.20 creating admin pannel
+    path('summernote/', include('django_summernote.urls')),
 ]
