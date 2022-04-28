af957a41161ab18db9aa2b9386b8747c053f20f9
mjjstockman <mjjstockman@gmail.com>
2022-04-28 08:34:19 +0000

    Add setlis/views, create static/css

--- setlist/views.py
+++ setlist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Setlist
 # Create your views here.
+
+def get_setlists(request):
+    setlists = Setlist.objects.all()
+    context = {
+        'setlists': setlists
+    }
+    return render(request, 'setlist/setlists', context)
