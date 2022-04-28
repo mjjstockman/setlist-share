8107898ff0fc493fcd5688ddb15a8d78d11052c2
mjjstockman <mjjstockman@gmail.com>
2022-04-28 14:50:06 +0000

    Add SetlistForm

---
+++ setlist/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Setlist
+
+
+class SetlistForm(forms.ModelForm):
+    class Meta:
+        model = Setlist
+        fields = ['gig', 'song']
--- setlist/views.py
+++ setlist/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Setlist
+from .forms import SetlistForm
 # Create your views here.
 
 def get_setlists(request):
     setlists = Setlist.objects.all()
+    form = SetlistForm()
     context = {
-        'setlists': setlists
+        'setlists': setlists,
+        'form': form
     }
     return render(request, 'setlist/setlists.html', context)
